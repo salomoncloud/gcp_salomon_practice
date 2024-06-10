d35c94b8eec7c47d5fdd31f004f627e92f81dcba
salomoncloud <158000174+salomoncloud@users.noreply.github.com>
2024-06-10 19:39:02 -0400

    Create provider.tf

---
+++ provider.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    google-beta = {
+      source  = "hashicorp/google-beta"
+      version = "~> 4.0"
+    }
+  }
+}
+provider "google" {
+}
+
+provider "google-beta" {
+}
